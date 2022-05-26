3e859a60a3a26c67f9f38bf9993458442a2b5e54
williansumida <sumidawillian@gmail.com>
2022-05-25 22:26:50 -0300

    fix navbar

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/services/autenticacao.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,19 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,
+    private serviceUser: UserService,
+    private auth: AutenticacaoService) { }
+
+  token = this.auth.autenticado()
 
   ngOnInit(): void {
+  }
+  deslogar() {
+    sessionStorage.setItem('token', null)
+    sessionStorage.setItem('expiry', null)
+    this.serviceUser.showMessage('Até a próxima!')
+    this.router.navigate(['/login'])
   }
 
 }
