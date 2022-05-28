d778c9376737c692825e21dc9fcb5de748a4bdc0
williansumida <sumidawillian@gmail.com>
2022-05-28 11:43:23 -0300

    exibir medico e paciente nome no consultas

--- src/app/components/consulta/consulta-read/consulta-read.component.ts
+++ src/app/components/consulta/consulta-read/consulta-read.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Consulta } from 'src/app/model/consulta.model';
+import { Medico } from 'src/app/model/medico.model';
+import { Paciente } from 'src/app/model/paciente.model';
 import { AutenticacaoService } from 'src/app/services/autenticacao.service';
 import { ConsultaService } from 'src/app/services/consulta.service';
+import { MedicoService } from 'src/app/services/medico.service';
+import { PacienteService } from 'src/app/services/paciente.service';
 
 @Component({
   selector: 'app-consulta-read',
@@ -9,21 +13,44 @@ import { ConsultaService } from 'src/app/services/consulta.service';
   styleUrls: ['./consulta-read.component.css']
 })
 export class ConsultaReadComponent implements OnInit {
-
+  paciente: Paciente[]
+  medico: Medico[]
   consultas: Consulta[]
   displayedColumns = ['id', 'idPaciente', 'idMedico', 'data', 'action']
 
   constructor(private serviceConsulta: ConsultaService,
+    private serviceMedico: MedicoService,
+    private servicePaciente: PacienteService,
     private auth: AutenticacaoService) { }
 
   ngOnInit(): void {
     if (this.auth.autenticado()) {
+      this.servicePaciente.getAllPacientes().subscribe(paciente => {
+        this.paciente = paciente.map(function (e) {
+          return { "id": e.id, "nome": e.nome, "dataNascimento": e.dataNascimento, "dataCadastro": e.dataCadastro }
+        })
+      })
+
+      this.serviceMedico.getAllMedicos().subscribe(medico => {
+        this.medico = medico.map(function (e) {
+          return { "id": e.id, "nome": e.nome, "data": e.dataCadastro, "idEspecialidade": e.idEspecialidade }
+        })
+      })
+
       this.serviceConsulta.getAllConsultas().subscribe(consultas => {
         this.consultas = consultas.map(function (e) {
           return { "id": e.id, "idPaciente": e.idPaciente, "idMedico": e.idMedico, "data": e.data }
         })
       })
     }
+  }
+
+  getNamePaciente(id: number){
+    return ( this.paciente.filter(paciente => paciente.id == id)[0].nome ) 
+  }
+
+  getNameMedico(id: number){
+    return ( this.medico.filter(medico => medico.id == id)[0].nome ) 
   }
 
 }
